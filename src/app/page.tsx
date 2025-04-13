7c7ab74729879d88cac32de45ab644481955d6a3
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2025-04-13 15:28:11 +0600

    hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
-import React from "react";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>The power of legal protection</h1>
+      <div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam quae
+            quaerat dolor! Quisquam possimus alias odio, voluptatum impedit
+            molestias? Exercitationem dicta nemo ipsa harum! Perferendis aliquam
+            ullam optio autem iste.
+          </p>
+          <button>
+            Get Consultation
+          </button>
+        </div>
+        <div>
+          video
+        </div>
+      </div>
+    </div>
+  );
+};
